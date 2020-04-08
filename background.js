83aa54d815d02ce9169c824de2935fc7524a320a
shohey1226 <shoheik@cpan.org>
2020-04-08 01:18:22 +0000

    updated logic and clean up a bit

--- background.js
+++ background.js
@@ -1,87 +1,11 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not
-// use this file except in compliance with the License. You may obtain a copy of
-// the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations under
-// the License.
-
-chrome.commands.onCommand.addListener(function(command) {
-  //console.log(command);
-  //var event = {};
-  //if (command === "ctrl-p") {
-  //  event = {
-  //    key: "p",
-  //    ctrlKey: true,
-  //    shiftKey: false,
-  //    altKey: false,
-  //    metaKey: false,
-  //    repeat: false,
-  //    isComposing: false,
-  //    charCode: 0,
-  //    keyCode: 80
-  //  };
-  //} else if (command === "ctrl-n") {
-  //  event = {
-  //    key: "n",
-  //    ctrlKey: true,
-  //    shiftKey: false,
-  //    altKey: false,
-  //    metaKey: false,
-  //    repeat: false,
-  //    isComposing: false,
-  //    charCode: 0,
-  //    keyCode: 78
-  //  };
-  //} else if (command === "ctrl-t") {
-  //  event = {
-  //    key: "t",
-  //    ctrlKey: true,
-  //    shiftKey: false,
-  //    altKey: false,
-  //    metaKey: false,
-  //    repeat: false,
-  //    isComposing: false,
-  //    charCode: 0,
-  //    keyCode: 84
-  //  };
-  //}
 // https://stackoverflow.com/questions/17567624/pass-a-parameter-to-a-content-script-injected-using-chrome-tabs-executescript 
- chrome.tabs.executeScript(null, {
-   code: `var command = "${command}"`
- }, function(){
-   chrome.tabs.executeScript(
-     null,
-     {file: "content.js"}
-     //function(results){
-     //  console.log(result)
-     //}
-   );
- });
-  //chrome.tabs.executeScript(
-  //  null,
-  //  {code: "var x = " + JSON.stringify(document.) + "; x"}, 
-  //  function(result){
-  //     var event = {
-  //       key: "n",
-  //       ctrlKey: true,
-  //       shiftKey: false,
-  //       altKey: false,
-  //       metaKey: false,
-  //       repeat: false,
-  //       isComposing: false,
-  //       charCode: 0,
-  //       keyCode: 78
-  //     };
-  //    console.log(result);
-  //    var keyEvent = new KeyboardEvent('keydown', event); 
-  //    //document.dispatchEvent(keyEvent);
-  //    //alert(result[0])
-  //  });
+chrome.commands.onCommand.addListener(function(command) {
+  chrome.tabs.executeScript(null, {
+    code: `var command = "${command}"`
+  }, function(){
+    chrome.tabs.executeScript(
+      null,
+      {file: "content.js"}
+    );
+  });
 });
--- content.js
+++ content.js
@@ -1,18 +1,38 @@
-console.log('hoge');
-console.log(command);
-console.log(window.term);
-console.log(document.getElementsByClassName('xterm-helper-textarea'));
-var termTextarea = document.getElementsByClassName('xterm-helper-textarea')[0];
-var event = {
-  key: "n",
-  ctrlKey: true,
-  shiftKey: false,
-  altKey: false,
-  metaKey: false,
-  repeat: false,
-  isComposing: false,
-  charCode: 0,
-  keyCode: 78
-};
-var keyEvent = new KeyboardEvent('keydown', event); 
-termTextarea.dispatchEvent(keyEvent);
+//console.log(command);
+//console.log(document.getElementsByClassName('xterm-helper-textarea'));
+var termTextarea = document.getElementsByClassName("xterm-helper-textarea")[0];
+var event = null;
+
+switch (command) {
+  case "ctrl-n":
+    event = {
+      key: "n",
+      ctrlKey: true,
+      shiftKey: false,
+      altKey: false,
+      metaKey: false,
+      repeat: false,
+      isComposing: false,
+      charCode: 0,
+      keyCode: 78
+    };
+    break;
+  case "ctrl-t":
+    event = {
+      key: "t",
+      ctrlKey: true,
+      shiftKey: false,
+      altKey: false,
+      metaKey: false,
+      repeat: false,
+      isComposing: false,
+      charCode: 0,
+      keyCode: 84
+    };
+    break;
+}
+
+if (event) {
+  var keyEvent = new KeyboardEvent("keydown", event);
+  termTextarea.dispatchEvent(keyEvent);
+}
