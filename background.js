035f19da437d64929d95e8126943320ba6e2d9b9
shohey1226 <shoheik@cpan.org>
2020-09-25 06:47:50 +0000

    added for none wazaterm

--- background.js
+++ background.js
@@ -1,11 +1,28 @@
 // https://stackoverflow.com/questions/17567624/pass-a-parameter-to-a-content-script-injected-using-chrome-tabs-executescript 
 chrome.commands.onCommand.addListener(function(command) {
-  chrome.tabs.executeScript(null, {
-    code: `var command = "${command}"`
-  }, function(){
-    chrome.tabs.executeScript(
-      null,
-      {file: "content.js"}
-    );
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    var activeTabUrl = activeTab.url; 
+
+    if(activeTabUrl.match(/https:\/\/.*\.wazaterm\.com/)){
+      chrome.tabs.executeScript(null, {
+        code: `var command = "${command}"`
+      }, function(){
+        chrome.tabs.executeScript(
+          null,
+          {file: "content.js"}
+        );
+      });
+    }else if(activeTabUrl.match(/chrome-untrusted:\/\/terminal\/html\/terminal\.html/)){
+        // we should simulate ctrl-n and ctrl-t for terminal app in chromeos but we don't know how
+    }else{
+      if(command === "ctrl-n"){
+        chrome.windows.create();
+      } else if(command === "ctrl-t"){
+        chrome.tabs.create({});
+      }
+    }
+
   });
 });
